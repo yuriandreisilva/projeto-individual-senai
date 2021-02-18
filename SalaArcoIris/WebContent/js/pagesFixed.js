80d983b914f64f85e40743a689cd4e1ff8ff927e
Yuri Andrei <yuri.andrei@facedigital.com.br>
2021-02-18 18:12:11 -0300

    reformulacao paginas - bootstrap

---
+++ SalaArcoIris/WebContent/js/pagesFixed.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	$(".head").load("fixed/head.html")
+});
+
+$(document).ready(function(){
+	$("header").load("fixed/header.html")
+});
+
+$(document).ready(function(){
+	$("footer").load("fixed/footer.html")
+});
+
+$(document).ready(function(){
+	// 	$("#check").change(function() {
+	// 		if(this.checked) {
+	// 		   $(".corpo").addClass("corpo-menor");
+	// 		}else{
+	// 		   $(".corpo").removeClass("corpo-menor");
+	// 		}
+	// 	});
+	// });
+})
+
+
+
+	
+				
